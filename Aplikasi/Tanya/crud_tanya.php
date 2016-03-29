d212604cb93481dc5d6f6633a85d6fd2deee297a
Amin PO Muar <helmi007@yahoo.com>
2016-03-29 16:36:56 +0800

    tambah class crud_tanya.php

---
+++ Aplikasi/Tanya/crud_tanya.php
@@ -0,0 +1,50 @@
+<?php
+namespace Aplikasi\Tanya; //echo __NAMESPACE__; 
+class Crud_Tanya extends \Aplikasi\Kitab\Tanya
+{
+#==========================================================================================
+	public function __construct() 
+	{
+		parent::__construct();
+	}
+	
+	public function medanUbah2($cariID)
+	{
+		$senaraiMedan = 'id,Nama_Penuh,email,nohp';
+		return $senaraiMedan;
+	}
+	
+	public function medanUbah($cariID) 
+	{ 
+		# Set pemboleubah
+		# buat link
+		$alamat1 = 'http://xxx/crud/ubah2/",kp,"/'.$cariID.'/2010/2015/'; 
+		$url1 = '" <a target=_blank href=' . $alamat1 . '>SEMAK 1</a>"';
+		$url2 = 'concat("<a target=_blank href=' . $alamat1 . '>SEMAK 2</a>")';
+		# Set pemboleubah untuk sql
+        $senaraiMedan = 'id,'
+			. 'concat_ws("|",nama,operator,' . $url1 . ',' . $url2 . ') nama,'
+			. ' concat_ws("|",' . "\r"
+			. ' 	concat_ws("="," hasil",format(hasil,0)),' . "\r"
+			. ' 	concat_ws("="," belanja",format(belanja,0)),' . "\r"
+			. ' 	concat_ws("="," gaji",format(gaji,0)),' . "\r"
+			. ' 	concat_ws("="," aset",format(aset,0)),' . "\r"
+			. ' 	concat_ws("="," staf",format(staf,0)),' . "\r"
+			. ' 	concat_ws("="," stok akhir",format(stok,0))' . "\r"
+ 			. ' ) as data5P,'
+			. ' concat_ws("|",' . "\r"
+			. ' 	concat_ws("="," tel",tel),' . "\r"
+			. ' 	concat_ws("="," fax",fax),' . "\r"
+			. ' 	concat_ws("="," orang",orang),' . "\r"
+			. ' 	concat_ws("="," notel",notel),' . "\r"
+			. ' 	concat_ws("="," nofax",nofax)' . "\r"
+ 			. ' ) as dataHubungi,'
+			. 'concat_ws(" ",alamat1,alamat2,poskod,bandar) as alamat,' . "\r"
+			//. 'concat_ws(" ",no,batu,( if (jalan is null, "", concat("JALAN ",jalan) ) ),tmn_kg,poskod,dp_baru) alamat_baru,' . "\r"
+			. 'tel,notel,fax,nofax,responden,orang,email,esurat,'
+			. 'hasil,belanja,gaji,aset,staf,stok' . "\r" 
+			. '';	
+		return $senaraiMedan;
+	}	
+#==========================================================================================
+}
