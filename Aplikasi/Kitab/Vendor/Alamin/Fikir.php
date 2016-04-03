8a9c4c1e9814c9fa628e24507fe3b9529cd33f79
Amin PO Muar <helmi007@yahoo.com>
2016-04-03 14:58:35 +0800

    tambah fail untuk Vendor/Alamin/Fikir.php dan vendor/Sultanjdt/Fikir.php

---
+++ Aplikasi/Kitab/Vendor/Alamin/Fikir.php
@@ -0,0 +1,13 @@
+<?php
+//namespace Aplikasi\Kitab; //echo __NAMESPACE__; 
+namespace Aplikasi\Kitab\Vendor\Alamin; //echo __NAMESPACE__; 
+class Fikir
+{
+#= utama =========================================================================================
+	function umat()
+	{
+		echo 'masa meninggal dunia masih ingat atas umatnya';
+	}
+#= lain2 =========================================================================================
+#= tamat =========================================================================================
+}
---
+++ Aplikasi/Kitab/Vendor/Sultanjdt/Fikir.php
@@ -0,0 +1,13 @@
+<?php
+//namespace Aplikasi\Kitab; //echo __NAMESPACE__; 
+namespace Aplikasi\Kitab\Vendor\Sultanjdt; //echo __NAMESPACE__; 
+class Fikir
+{
+#= utama =========================================================================================
+	function umat()
+	{
+		echo 'rakyat negeri johor';
+	}
+#= lain2 =========================================================================================
+#= tamat =========================================================================================
+}
