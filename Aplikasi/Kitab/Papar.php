889a37b3f7c49f0a0c7d57d1c4e4c8ae358bd733
Amin PO Muar <helmi007@yahoo.com>
2016-04-10 17:03:20 +0800

    tambah fungsi paparTemplate, bacaTemplate, pilihTemplate

--- Aplikasi/Kitab/Papar.php
+++ Aplikasi/Kitab/Papar.php
@@ -77,5 +77,83 @@ class Papar
 		}
 //*/		
 	}
+#==========================================================================================
+#------------------------------------------------------------------------------------------
+	public function paparTemplate($nama, $template, $noInclude = false)
+	{
+		$namafail = explode('/', $nama);
+        $failPapar = GetMatchingFiles(
+			GetContents(PAPAR . '/' . $namafail[0]),
+			$namafail[1] . '.php');
+		$paparFail = $failPapar[0];
+		$lokasiTemplate = '/template/' . $template;
+		
+		/*echo '<hr size=2>PAPAR=' . PAPAR . '<br>';
+		//echo '$namafail=<pre>'; print_r($namafail) . '</pre><br>';
+		//echo '$failPapar=<pre>'; print_r($failPapar) . '</pre><br>';
+		echo '$paparFail->' . $paparFail . '<br>'
+			. '$template->' . $template . '<br>' 
+			. '$lokasiTemplate->' . $lokasiTemplate . '<br>'
+			. '$noInclude->' . $noInclude . '<br>'
+			. '';//*/
+		
+		echo '<hr>require ' . PAPAR . $lokasiTemplate . '/diatas.php'
+			. '<br>require ' . PAPAR . $lokasiTemplate . '/menu_atas.php'
+			. '<br>require ' . $paparFail
+			. '<br>require ' . PAPAR . $lokasiTemplate . '/dibawah.php'
+			. '';//*/
+	}
+#------------------------------------------------------------------------------------------
+	public function bacaTemplate($nama, $template, $noInclude = false)
+	{
+		$namafail = explode('/', $nama);
+        $failPapar = GetMatchingFiles(
+			GetContents(PAPAR . '/' . $namafail[0]),
+			$namafail[1] . '.php');
+		$paparFail = $failPapar[0];
+		$lokasiTemplate = '/template/' . $template;
+		
+		/*echo '<hr size=2>PAPAR=' . PAPAR . '<br>';
+		//echo '$namafail=<pre>'; print_r($namafail) . '</pre><br>';
+		//echo '$failPapar=<pre>'; print_r($failPapar) . '</pre><br>';
+		echo '$paparFail->' . $paparFail . '<br>'
+			. '$template->' . $template . '<br>' 
+			. '$lokasiTemplate->' . $lokasiTemplate . '<br>'
+			. '$noInclude->' . $noInclude . '<br>'
+			. '';//*/
+		
+		if ($noInclude == true) 
+		{
+			require PAPAR . $nama . '.php';	
+		}
+		else 
+		{
+			require PAPAR . $lokasiTemplate . '/diatas.php';
+			require PAPAR . $lokasiTemplate . '/menu_atas.php';
+			require PAPAR . $lokasiTemplate . '/menu-tengah-atas.php';
+			require $paparFail;
+			require PAPAR . $lokasiTemplate . '/menu-tengah-bawah.php';
+			require PAPAR . $lokasiTemplate . '/dibawah.php';			
+		}//*/
+	}
+#-----------------------------------------------------------------------------------------
+	function pilihTemplate($template = 0)
+	{
+	#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#		
+		switch ($template)
+		{
+			case 2: $jenis = 'startbootstrap-sb-admin-2-1.0.8';
+			break;
 
+			case 1: $jenis = 'FlatAdmin-V.2';
+			break;
+
+			default: $jenis = 'AdminLTE-2.3.0';
+			break;
+		}
+	#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#	
+		return $jenis;
+	}
+#-----------------------------------------------------------------------------------------
+#==========================================================================================
 }
